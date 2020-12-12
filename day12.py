7f5a859afc445a448de367d54ad20c22a5b8919c
Rasmus Öhman Bruno <rasmus@ohmanbruno.se>
2020-12-12 12:30:15 +0100

    day 12 complex solution

--- day12.py
+++ day12.py
@@ -1,32 +1,26 @@
 from fileinput import input
-import time
-import curses
-import math
+import cmath
 
-SPEED = 2
-directions = ['N', 'E', 'S', 'W']
-nwse = {'N': (0, -1), 'S': (0, 1), 'W': (-1, 0), 'E': (1, 0)}
-pos = [0, 0]
-waypoint = [10, -1]
+nwse = {'N': 1j, 'S': -1j, 'W': -1, 'E': 1}
 
+pos = 0
+waypoint = 10+1j
 for line in input('data/day12.txt'):
     instruction = line[0]
-    amount = int(line[1:])
+    units = int(line[1:])
     if instruction in nwse:
-        waypoint[0] += nwse[instruction][0]*amount
-        waypoint[1] += nwse[instruction][1]*amount
-    elif instruction == 'R' or instruction == 'L':
-        angle = amount*(2*math.pi)/360
-        angle = angle if instruction == 'R' else -angle
-        waypoint = [waypoint[0]*round(math.cos(angle)) -
-                    waypoint[1]*round(math.sin(angle)),
-                    waypoint[0] * round(math.sin(angle)) +
-                    waypoint[1]*round(math.cos(angle))]
+        waypoint += nwse[instruction]*units
+    elif instruction == 'R':
+        angle = -units*(2*cmath.pi)/360
+        waypoint *= cmath.exp(angle*1j)
+    elif instruction == 'L':
+        angle = units*(2*cmath.pi)/360
+        waypoint *= cmath.exp(angle*1j)
     elif instruction == 'F':
-        pos[0] += waypoint[0]*amount
-        pos[1] += waypoint[1]*amount
+        pos += waypoint*units
     else:
         raise ValueError('aah')
     print(
-        f'instruction:{line.rstrip()}\t waypoint:{waypoint}\t position:{pos}')
-print(f'total:{abs(pos[0])+abs(pos[1])}')
+        f'instruction:{line.rstrip():6}\t waypoint:{waypoint:6.0f}\t position:{pos:6.0f}')
+total = round(abs(pos.real)+abs(pos.imag))
+print(f'total:{total}')
